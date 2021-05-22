19d659638fc74eab254793c9b08ba1ec31dea044
leandropaixao <leandro.tec.inf@gmail.com>
2021-05-22 14:51:53 -0300

    Adicionada a classe de Enum para os Generos

---
+++ Enum/Genero.cs
@@ -0,0 +1,15 @@
+namespace DIO_AppCadastroSeries_DotNet
+{
+    public enum Genero
+    {
+        Acao = 1,
+        Aventura = 2,
+        Comedia = 3,
+        Ficcao = 4,
+        Romance = 5,
+        Terror = 6,
+        Suspense = 7,
+        Anime = 8,
+        Desenho = 9        
+    }
+}
